c0c487f2049cd6ecaed6fd6e63212f9a61d3a515
Manuel Peise <manuel.p80@gmx.de>
2023-12-14 22:05:56 +0100

    adding side menu as offcanvas

---
+++ sources/Wasm-Tool-Box/Data.Models/UI/SideMenu/SideMenuModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Data.Models.UI.SideMenu
+{
+    public class SideMenuModel
+    {
+        public string? Header { get; set; }
+        public List<SideMenuItemModel> MenuItems { get; set; } = new List<SideMenuItemModel>();
+    }
+}
---
+++ sources/Wasm-Tool-Box/Web.Core/Client/Shared/Components/Menus/SideMenuItem.razor.cs
@@ -0,0 +1,11 @@
+﻿using Data.Models.UI.SideMenu;
+using Microsoft.AspNetCore.Components;
+
+namespace Web.Core.Client.Shared.Components.Menus
+{
+    public partial class SideMenuItem: ComponentBase
+    {
+        [Parameter]
+        public SideMenuItemModel ItemModel { get; set; } = new SideMenuItemModel();
+    }
+}
